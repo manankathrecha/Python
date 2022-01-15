7ad496f034395cd8552c2a0c1c393d8d7fb719d5
manankathrecha <65075164+manankathrecha@users.noreply.github.com>
2022-01-15 12:03:58 +0530

    Add files via upload

---
+++ t1.py
@@ -0,0 +1,6 @@
+# Write a Python program to create a tuple with different data types.
+tuple = ('Manan', True, 3.14)
+print(tuple)
+
+# D21CE167
+# Manan Kathrecha
---
+++ t2.py
@@ -0,0 +1,7 @@
+# Write a Python program to create a tuple with numbers and print one item.
+tuple=(1,2,3,4,5)
+print(tuple[2])
+print(tuple[-1])
+
+# D21CE167
+# Manan Kathrecha
