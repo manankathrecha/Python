2518735a08be7ae4de1224d6b62ff86143aa3d62
manankathrecha <65075164+manankathrecha@users.noreply.github.com>
2022-01-28 10:17:15 +0530

    Update practical3.py

--- practical3.py
+++ practical3.py
@@ -1,4 +1,8 @@
+# Aim: Find captain room number
 k = int(input())
 room_number = list(map(int, input().split()))
 captain_room_number = (sum(set(room_number)) * k - sum(room_number)) // (k - 1)
 print(captain_room_number)
+
+# D21CE167
+# Manan Kathrecha
